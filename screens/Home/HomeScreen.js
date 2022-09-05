95b060f19fb64d1118340b3334abbbbc6e98702b
Apungu Raymond <apunguray@gmail.com>
2022-09-05 18:24:09 +0800

    initial commit

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
+import { StatusBar } from "expo-status-bar"; 
 import React from 'react'
 
 const HomeScreen = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1">
       <Text>HomeScreen</Text>
-    </View>
+      <StatusBar style="dark" translucent={false} backgroundColor={"#fff"}/>
+    </SafeAreaView>
   )
 }
 
