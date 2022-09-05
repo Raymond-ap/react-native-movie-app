3394d4a7a14a85e5f417f536e44d229328b0d578
Apungu Raymond <apunguray@gmail.com>
2022-09-05 18:23:55 +0800

    clean up

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -9,7 +7,6 @@ import {
 } from './screens'
 
 const Stack = createNativeStackNavigator()
-
 
 export default function App() {
   return (
