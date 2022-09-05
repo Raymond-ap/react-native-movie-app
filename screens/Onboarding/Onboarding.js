344023c6ee0be4fd8bc43e9f4d5c93f334deff1a
Apungu Raymond <apunguray@gmail.com>
2022-09-05 18:36:44 +0800

    local update

--- screens/Onboarding/Onboarding.js
+++ screens/Onboarding/Onboarding.js
@@ -1,17 +1,22 @@
 import {
-  View,
-  Text,
   ImageBackground,
   StatusBar,
   SafeAreaView,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
 
 const Onboarding = () => {
   const navigation = useNavigation();
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Home");
+    }, 2000);
+  }, []);
+
   return (
     <ImageBackground
       source={require("../../assets/banner.jpeg")}
@@ -20,19 +25,7 @@ const Onboarding = () => {
       <LinearGradient
         className="absolute top-0 bottom-0 right-0 left-0"
         colors={["rgba(0,0,0,0.8)", "rgba(0,0,0,0.9)"]}
-      >
-        <SafeAreaView className="w-full absolute bottom-6">
-          <TouchableOpacity
-            onPress={() => navigation.navigate("Home")}
-            activeOpacity={0.9}
-            className="mx-4 py-3 bg-red-600 rounded-xl shadow-md items-center justify-between"
-          >
-            <Text className="text-lg text-white font-bold capitalize tracking-wider">
-              explore now
-            </Text>
-          </TouchableOpacity>
-        </SafeAreaView>
-      </LinearGradient>
+      ></LinearGradient>
       <StatusBar
         barStyle={"light-content"}
         translucent={true}
