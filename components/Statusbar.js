f1fec96272259eda070bcd755ea48738170a62e5
Apungu Raymond <apunguray@gmail.com>
2022-09-05 18:47:11 +0800

    custome Statusbar component

---
+++ components/Statusbar.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+export default function Statusbar() {
+  return (
+    <StatusBar style="light" translucent={false} backgroundColor={"#0f172a"} />
+  );
+}
