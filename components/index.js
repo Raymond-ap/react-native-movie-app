887fd0aacba4ac1e02571d22712ba22c1eac7931
Apungu Raymond <apunguray@gmail.com>
2022-09-09 03:47:45 +0800

    local update

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import AppStatusbar from "./Statusbar";
 import Header from "./Header";
 import FeaturedRow from './FeaturedRow'
+import Movie from "./Movie";
 
-export { AppStatusbar, Header, FeaturedRow };
+export { AppStatusbar, Header, FeaturedRow, Movie };
