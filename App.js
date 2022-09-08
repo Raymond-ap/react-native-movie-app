cfc03f66c6550d57cc14200a809dc2453f17e494
Apungu Raymond <apunguray@gmail.com>
2022-09-09 04:15:13 +0800

    render detail screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { TailwindProvider } from "tailwindcss-react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { Onboarding, HomeScreen } from "./screens";
+import { Onboarding, HomeScreen, MovieDeatil } from "./screens";
 import { AuthProvider } from "./hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
@@ -17,7 +17,16 @@ export default function App() {
           >
             <Stack.Screen name="Onboarding" component={Onboarding} />
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Group></Stack.Group>
+            <Stack.Group>
+              <Stack.Screen
+                screenOptions={{
+                  animation: "slide_from_right",
+                  animationTypeForReplace: "pop",
+                }}
+                name="MovieDeatil"
+                component={MovieDeatil}
+              />
+            </Stack.Group>
           </Stack.Navigator>
         </AuthProvider>
       </NavigationContainer>
