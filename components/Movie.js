102810ac3a13acfbcf001ef63b2cf9dd1957733d
Apungu Raymond <apunguray@gmail.com>
2022-09-09 04:02:02 +0800

    render movies

--- components/Movie.js
+++ components/Movie.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, FlatList, TouchableOpacity } from "react-native";
+import React from "react";
 
-const Movie = () => {
+const Movie = ({ data }) => {
   return (
-    <View>
-      <Text>Movie</Text>
-    </View>
-  )
-}
+    <FlatList
+      data={data}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={(_, index) => index.toString()}
+      renderItem={({ item }) => (
+        <TouchableOpacity activeOpacity={0.9} className="h-48 w-32 mx-2">
+          <Image 
+          source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
+          className="h-full w-full object-cover rounded-lg" />
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
 
-export default Movie
+export default Movie;
