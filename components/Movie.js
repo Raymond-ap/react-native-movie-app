56e0214113fb82a07d043a324e6e74554770e4b9
Apungu Raymond <apunguray@gmail.com>
2022-09-09 04:05:58 +0800

    local changes

--- components/Movie.js
+++ components/Movie.js
@@ -4,15 +4,18 @@ import React from "react";
 const Movie = ({ data }) => {
   return (
     <FlatList
-      data={data}
+      data={data.slice(0, 15)}
       horizontal
       showsHorizontalScrollIndicator={false}
       keyExtractor={(_, index) => index.toString()}
       renderItem={({ item }) => (
         <TouchableOpacity activeOpacity={0.9} className="h-48 w-32 mx-2">
-          <Image 
-          source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
-          className="h-full w-full object-cover rounded-lg" />
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+            }}
+            className="h-full bg-slate-900 w-full object-cover rounded-lg"
+          />
         </TouchableOpacity>
       )}
     />
