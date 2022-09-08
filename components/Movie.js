aecfe7c66831220421dd266ea9a43bac7a63220a
Apungu Raymond <apunguray@gmail.com>
2022-09-09 03:47:54 +0800

    local update

---
+++ components/Movie.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Movie = () => {
+  return (
+    <View>
+      <Text>Movie</Text>
+    </View>
+  )
+}
+
+export default Movie
