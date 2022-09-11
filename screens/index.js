0c0d8be6bc8887df5eb744dd04e99dfe651dfc3d
Apungu Raymond <apunguray@gmail.com>
2022-09-11 15:39:09 +0800

    local update

--- screens/index.js
+++ screens/index.js
@@ -1,9 +1,11 @@
 import Onboarding from "./Onboarding/Onboarding";
 import HomeScreen from "./Home/HomeScreen";
 import MovieDeatil from "./Details/MovieDeatil";
+import ListingScreen from "./Listings/ListingScreen";
 
 export {
     Onboarding,
     HomeScreen,
-    MovieDeatil
+    MovieDeatil,
+    ListingScreen
 }
