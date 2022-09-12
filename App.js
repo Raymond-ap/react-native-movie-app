65efeda7f5dd86b67781be861e8b190f22cc8b0a
Apungu Raymond <apunguray@gmail.com>
2022-09-13 05:02:52 +0800

    default theme

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { TailwindProvider } from "tailwindcss-react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import { Onboarding, HomeScreen, MovieDeatil } from "./screens";
+import { NavigationContainer, DarkTheme } from "@react-navigation/native";
+import { Onboarding, HomeScreen, MovieDeatil, ListingScreen } from "./screens";
 import { AuthProvider } from "./hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +9,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <TailwindProvider>
-      <NavigationContainer>
+      <NavigationContainer theme={DarkTheme} className="bg-black flex-1">
         <AuthProvider>
           <Stack.Navigator
             initialRouteName="Onboarding"
@@ -17,11 +17,11 @@ export default function App() {
           >
             <Stack.Screen name="Onboarding" component={Onboarding} />
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Listing" component={ListingScreen} />
             <Stack.Group>
               <Stack.Screen
                 screenOptions={{
                   animation: "slide_from_right",
-                  animationTypeForReplace: "pop",
                 }}
                 name="MovieDeatil"
                 component={MovieDeatil}
