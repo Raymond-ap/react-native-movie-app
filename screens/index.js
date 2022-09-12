6d0bb47ee261943555d0c2024b5548933b34663c
Apungu Raymond <apunguray@gmail.com>
2022-09-13 05:45:20 +0800

    global exports

--- screens/index.js
+++ screens/index.js
@@ -2,10 +2,12 @@ import Onboarding from "./Onboarding/Onboarding";
 import HomeScreen from "./Home/HomeScreen";
 import MovieDeatil from "./Details/MovieDeatil";
 import ListingScreen from "./Listings/ListingScreen";
+import SarchScreen from "./Search/SarchScreen";
 
 export {
     Onboarding,
     HomeScreen,
     MovieDeatil,
-    ListingScreen
+    ListingScreen,
+    SarchScreen
 }
